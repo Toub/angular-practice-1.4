a735d909fc3b79233cd87a75ab94cdae090560b9
Toub <n.toublanc@gmail.com>
2015-09-06 15:24:18 +0200

    Init tp5

--- app/app.js
+++ app/app.js
@@ -4,4 +4,30 @@
 
     angular.module('tpAngular', []);
 
+    angular.module('tpAngular').controller('TpaProfileUserEditController', TpaProfileUserEditController);
+
+    function TpaProfileUserEditController($scope, $log) {
+
+        // public attributes
+        $scope.user = {
+            firstName: 'John'
+        };
+
+        // public methods
+        $scope.getFullName = getFullName;
+        
+        // initialization
+        init();
+
+        function init() {
+            $log.info('Initializing controller...');
+        }
+        
+        function getFullName(){
+            // TODO
+        }
+        
+    }
+
+
 })();
