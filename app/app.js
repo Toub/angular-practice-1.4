474513447b63db5f386d5d02df387d4d6d8ab531
Toub <n.toublanc@gmail.com>
2015-09-07 14:58:07 +0200

    First route

--- app/app.js
+++ app/app.js
@@ -1,21 +1,23 @@
 (function () {
 
-        'use strict';
+    'use strict';
 
-        angular.module('tpAngular', ['tpAngular.profile', 'mgcrea.ngStrap', 'ngMessages', 'ui.router']);
+    angular.module('tpAngular', ['tpAngular.profile', 'mgcrea.ngStrap', 'ngMessages', 'ui.router']);
 
-        angular.module('tpAngular'.config(function ($stateProvider, $urlRouterProvider) {
+    angular.module('tpAngular').config(function ($stateProvider, $urlRouterProvider) {
 
-                // For any unmatched url, redirect to /state1
-                // TODO $urlRouterProvider.otherwise(...);
-
-                // Now set up the states
-                $stateProvider
-                    .state('edit-user', {
-                        url: "/profile/user/edit",
-                        templateUrl: 'profile/profile.user-edit.html',
-                        controller: 'TpaProfileUserEditController'
-                    });
+        // states configuration
+        $stateProvider
+            .state('edit-user', {
+                url: "/profile/user/edit",
+                templateUrl: 'profile/profile.user-edit.html',
+                controller: 'TpaProfileUserEditController'
             });
 
-        })();
+        // default route
+        $urlRouterProvider.otherwise('/profile/user/edit');
+
+
+    });
+
+})();
