caff55b1af51b0bbc758d6b65b915a44b17ccf31
Toub <n.toublanc@gmail.com>
2015-09-07 10:36:05 +0200

    init ui-router

--- app/app.js
+++ app/app.js
@@ -1,7 +1,21 @@
 (function () {
 
-    'use strict';
+        'use strict';
 
-    angular.module('tpAngular', ['tpAngular.profile', 'mgcrea.ngStrap', 'ngMessages']);
+        angular.module('tpAngular', ['tpAngular.profile', 'mgcrea.ngStrap', 'ngMessages', 'ui.router']);
 
-})();
+        angular.module('tpAngular'.config(function ($stateProvider, $urlRouterProvider) {
+
+                // For any unmatched url, redirect to /state1
+                // TODO $urlRouterProvider.otherwise(...);
+
+                // Now set up the states
+                $stateProvider
+                    .state('edit-user', {
+                        url: "/profile/user/edit",
+                        templateUrl: 'profile/profile.user-edit.html',
+                        controller: 'TpaProfileUserEditController'
+                    });
+            });
+
+        })();
